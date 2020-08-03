cbd02f79381c3104f40cee78dba2310ab2f992c7
Akhilesh Rai <arsrocz@gmail.com>
2020-08-03 16:52:00 +0530

    Create multiple_table.sql

---
+++ multiple_table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE people
+  (
+    first_name VARCHAR(20),
+    last_name VARCHAR(20),
+    age INT
+  );
+INSERT INTO people(first_name, last_name, age)
+VALUES ('Tina', 'Belcher', 13);
+INSERT INTO people(age, last_name, first_name)
+VALUES (42, 'Belcher', 'Bob');
+INSERT INTO people(first_name, last_name, age)
+VALUES('Linda', 'Belcher', 45)
+  ,('Phillip', 'Frond', 38)
+  ,('Calvin', 'Fischoeder', 70);
