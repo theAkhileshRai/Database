6a1340dc4fd2728a0ee6fdc2d247e51572f45565
Akhilesh Rai <arsrocz@gmail.com>
2021-01-05 06:46:48 +0530

    Create unique_cats2.sql

---
+++ MySQL/unique_cats2.sql
@@ -0,0 +1,9 @@
+
+CREATE TABLE unique_cats2(
+    cat_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(20),
+    age INT DEFAULT 9
+    
+    );
+
+INSERT INTO unique_cats2(name,age) VALUES('skippy',7);
